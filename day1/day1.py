8331a95ea2e596e53acf221f54f1d344c76cbfe6
Martin Dimmen <dimmenm@outlook.com>
2022-12-02 00:42:22 +0100

    day1

---
+++ day1/day1.py
@@ -0,0 +1,20 @@
+calories=0
+sumCalories=list()
+top3=0
+
+with open ("day1.txt","r") as input:
+    for i in input:
+        try:
+            caloriesint=int(i)
+            calories+=caloriesint
+        except ValueError:
+            sumCalories.append(calories)
+            calories=0
+
+sumCalories.sort()
+
+for i in sumCalories[-3:]:
+    top3+=i
+
+print(f"Elf with most calories {sumCalories[-1:]}")
+print(f"Top 3 elfs with most calories {top3}")
