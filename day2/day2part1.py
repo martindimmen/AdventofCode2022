c3471de9337d1d99bfa93d6032ebc61040a8dbf6
Martin Dimmen <dimmenm@outlook.com>
2022-12-03 01:17:02 +0100

    day2

--- day2/day2part1.py
+++ day2/day2part1.py
@@ -12,31 +12,30 @@ with open ("day2.txt") as file:
         columns=columns.split(" ")
         opponent=columns[0]
         me=columns[1]
-        #Stein og Stein
         if opponent=="A" and me=="X":
             points+=(rock+draw)
-        #Stein og Papir
+        
         elif opponent=="A" and me=="Y":
             points+=(rock+draw)
-        #Stein og Saks
+        
         elif opponent=="A" and me=="Z":
             points+=(rock+won)
-        #Papir og Stein
+        
         elif opponent=="B" and me=="X":
             points+=(paper+won) 
-        #Papir og papir
+        
         elif opponent=="B" and me=="Y":
             points+=(paper+draw)
-        #Papir og saks
+        
         elif opponent=="B" and me=="Z":
             points+=(paper+lost) 
-        #Saks og stein
+        
         elif opponent=="C" and me=="X":
             points+=(scissors+lost)
-        #Saks og Papir
+        
         elif opponent=="C" and me=="Y":
             points+=(scissors+won)
-        #Saks og saks
+        
         elif opponent=="C" and me=="Z":
             points+=(scissors+draw)   
 print(points)   
